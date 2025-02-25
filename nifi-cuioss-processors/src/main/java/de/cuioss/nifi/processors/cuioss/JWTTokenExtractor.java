7d0d2e9a75556b32f7423f8c28eba22723af7f30
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-02-25 21:11:29 +0100

    Restructuring

--- nifi-cuioss-processors/src/main/java/de/cuioss/nifi/processors/cuioss/JWTTokenExtractor.java
+++ nifi-cuioss-processors/src/main/java/de/cuioss/nifi/processors/cuioss/JWTTokenExtractor.java
@@ -16,16 +16,16 @@
  */
 package de.cuioss.nifi.processors.cuioss;
 
-import org.apache.nifi.components.PropertyDescriptor;
-import org.apache.nifi.flowfile.FlowFile;
+import lombok.Getter;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
 import org.apache.nifi.annotation.behavior.ReadsAttributes;
 import org.apache.nifi.annotation.behavior.WritesAttribute;
 import org.apache.nifi.annotation.behavior.WritesAttributes;
-import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.SeeAlso;
 import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
@@ -36,12 +36,12 @@ import org.apache.nifi.processor.util.StandardValidators;
 import java.util.List;
 import java.util.Set;
 
-@Tags({"example"})
-@CapabilityDescription("Provide a description")
+@Tags({"oauth,jwt"})
+@CapabilityDescription("Extracts a bearer token and verifies it against the configured issuer. In addition, it exposes the claims as metadata")
 @SeeAlso({})
-@ReadsAttributes({@ReadsAttribute(attribute="", description="")})
-@WritesAttributes({@WritesAttribute(attribute="", description="")})
-public class MyProcessor extends AbstractProcessor {
+@ReadsAttributes({@ReadsAttribute(attribute = "", description = "")})
+@WritesAttributes({@WritesAttribute(attribute = "", description = "")})
+public class JWTTokenExtractor extends AbstractProcessor {
 
     public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor
             .Builder()
@@ -54,33 +54,25 @@ public class MyProcessor extends AbstractProcessor {
 
     public static final Relationship REL_SUCCESS = new Relationship.Builder()
             .name("success")
-            .description("Example success relationship")
+            .description("Indicates that the token is valid")
             .build();
 
-    private List<PropertyDescriptor> descriptors;
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("Indicates that there was no valid bearer token found.")
+            .build();
 
+
+    @Getter
+    private List<PropertyDescriptor> supportedPropertyDescriptors;
+
+    @Getter
     private Set<Relationship> relationships;
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
-        descriptors = List.of(MY_PROPERTY);
-
-        relationships = Set.of(REL_SUCCESS);
-    }
-
-    @Override
-    public Set<Relationship> getRelationships() {
-        return this.relationships;
-    }
-
-    @Override
-    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-        return descriptors;
-    }
-
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-
+        supportedPropertyDescriptors = List.of(MY_PROPERTY);
+        relationships = Set.of(REL_SUCCESS, REL_FAILURE);
     }
 
     @Override
