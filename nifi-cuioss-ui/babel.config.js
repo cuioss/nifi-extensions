053d01825281e5e40069793feb0a35b534ae26e5
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-06-10 11:48:45 +0200

    Resolutes some findings

--- nifi-cuioss-ui/babel.config.js
+++ nifi-cuioss-ui/babel.config.js
@@ -2,11 +2,7 @@ module.exports = (api) => {
     const isTest = api.env('test'); // Automatically true when NODE_ENV=test (which Jest sets)
     const plugins = [];
 
-    if (isTest) {
-        // This plugin transforms AMD to CommonJS, which Jest can understand.
-        // It's useful if you have AMD modules that need to be tested.
-        // plugins.push('babel-plugin-transform-amd-to-commonjs'); // Removed
-    }
+    // If additional plugins are needed for test environment, add them here
 
     return {
         presets: [
