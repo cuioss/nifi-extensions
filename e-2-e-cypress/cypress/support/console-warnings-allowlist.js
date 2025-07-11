7090628f7eb4faad0acdcacaa7edc457e8e179ef
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-07-11 21:12:01 +0200

    Refactor e-2-e-playwright documentation to meet standards

--- e-2-e-cypress/cypress/support/console-warnings-allowlist.js
+++
@@ -1,8 +0,0 @@
-// Add only warnings that cannot be fixed
-const allowedWarnings = [
-  'Warning: validateDOMNesting(...): <div> cannot appear as a descendant of <p>.',
-  'DevTools failed to load source map',
-  'Content Security Policy violation for inline script',
-];
-
-export default allowedWarnings;
